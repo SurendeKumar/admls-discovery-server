ec6053dfe98d80731b8ce0ac3251dcc45f2f3c07
Surender Kumar <45120705+SurendeKumar@users.noreply.github.com>
2026-04-14 21:27:45 +0100

    Updated config server

--- src/main/java/com/admls_discoveryserver/AdmlsDiscoveryServerApplication.java
+++ src/main/java/com/admls_discoveryserver/AdmlsDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.admls_discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AdmlsDiscoveryServerApplication {
 
 	public static void main(String[] args) {

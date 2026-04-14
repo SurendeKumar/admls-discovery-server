bb9fe1a45fc846e7eee31775575920b939efede9
Surender Kumar <45120705+SurendeKumar@users.noreply.github.com>
2026-04-14 14:14:20 +0100

    Added jar package for discovery service
    3rd service for admls for server discovery - Eureka

---
+++ admls-discovery-server/src/main/java/com/admls_discoveryserver/AdmlsDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.admls_discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmlsDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmlsDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ admls-discovery-server/src/test/java/com/admls_discoveryserver/AdmlsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admls_discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmlsDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
